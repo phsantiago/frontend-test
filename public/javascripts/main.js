3b281d53888a5b5cd134ada5b51d4384fcb01427
phsantiago <pedroh_s_@hotmail.com>
2018-01-19 00:06:36 +0000

    WIP fetch and fix data

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,1 +1,40 @@
-// your code
+import '../stylesheets/style.scss';
+
+const fetch = (url, cb, err) => {
+  const xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4) {
+      if (this.status == 200) {
+        cb(xhttp.responseText);
+      } else {
+        typeof err === 'function' && err(xhttp);
+      }
+    }
+  };
+  xhttp.open("GET", url, true);
+  xhttp.send();
+}
+
+const get = key => obj => obj[key];
+
+const compose = (...args) => initial => args.reduceRight(
+  (result, fn) => fn(result),
+  initial
+);
+
+const castVotes = actorList => {
+  const fixed = actorList.map(actor => {
+    if(typeof actor.positive !== 'number') {
+      actor.positive = 0;
+    }
+    if(typeof actor.negative !== 'number') {
+      actor.negative = 0;
+    }
+    return actor;
+  })
+}
+
+const fixData = compose(castVotes, get('data'), JSON.parse)
+
+fetch("https://raw.githubusercontent.com/r7com/frontend-test/master/public/fazenda.json", fixData, console.error)
+
