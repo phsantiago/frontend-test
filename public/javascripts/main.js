7cad5a7c3e7f73ac6ae25d99aaad8fcd0d268713
phsantiago <pedroh_s_@hotmail.com>
2018-01-20 03:59:10 +0000

    WIP - getting percentage of casted votes done

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,40 +1,71 @@
-import '../stylesheets/style.scss';
-
 const fetch = (url, cb, err) => {
   const xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = () => {
+  xhttp.onreadystatechange = function ready() {
     if (this.readyState === 4) {
       if (this.status === 200) {
         cb(xhttp.responseText);
-      } else {
-        typeof err === 'function' && err(xhttp);
+      } else if (typeof err === 'function') {
+        err(xhttp);
       }
     }
   };
-  xhttp.open("GET", url, true);
+  xhttp.open('GET', url, true);
   xhttp.send();
-}
+};
 
 const get = key => obj => obj[key];
 
 const compose = (...args) => initial => args.reduceRight(
   (result, fn) => fn(result),
-  initial
+  initial,
 );
 
-const castVotes = actorList => {
-  const fixed = actorList.map(actor => {
-    if(typeof actor.positive !== 'number') {
-      actor.positive = 0;
-    }
-    if(typeof actor.negative !== 'number') {
-      actor.negative = 0;
-    }
-    return actor;
-  })
-}
+export const castActorVotes = (actor) => {
+  const castedActor = actor;
+  const positiveType = typeof actor.positive;
+  const negativeType = typeof actor.negative;
 
-const onReceiveJson = compose(votesIntoPercentage, castVotes, get('data'), JSON.parse)
+  if (positiveType !== 'number' && positiveType !== 'string') {
+    castedActor.positive = 0;
+  } else if (positiveType === 'string') {
+    castedActor.positive = parseInt(actor.positive, 10);
+  }
 
-fetch("https://raw.githubusercontent.com/r7com/frontend-test/master/public/fazenda.json", onReceiveJson, console.error)
+  if (negativeType !== 'number' && negativeType !== 'string') {
+    castedActor.negative = 0;
+  } else if (positiveType === 'string') {
+    castedActor.negative = parseInt(actor.negative, 10);
+  }
+
+  return castedActor;
+};
+
+const castActorList = actorList => actorList.map(castActorVotes);
+
+export const percentBetweenTwo = (a, b) => {
+  if (a === b) {
+    return 0;
+  } else if (a > b) {
+    return (b * 100) / a;
+  }
+  return (a * 100) / b;
+};
+
+export const votesIntoPercentage = data => data.map((actor) => {
+  const { positive, negative } = actor;
+  const total = positive + negative;
+  const transformedActor = actor;
+
+  transformedActor.positive = percentBetweenTwo(positive, total);
+  transformedActor.negative = percentBetweenTwo(negative, total);
+  return transformedActor;
+});
+
+export const comparePositive = (a, b) => b;
+
+export const sort = fn => arr => arr.sort(fn);
+
+const onReceiveJson = compose(votesIntoPercentage, castActorList, get('data'), JSON.parse);
+
+fetch('https://raw.githubusercontent.com/r7com/frontend-test/master/public/fazenda.json', onReceiveJson, console.error);
 
