2cb4ffc66cd4630a9e70d5e458899e5ba22b5468
phsantiago <pedroh_s_@hotmail.com>
2018-01-20 04:03:06 +0000

    WIP - fix string, null, missing from actor votes and % done

---
+++ public/javascripts/main.test.js
@@ -0,0 +1,92 @@
+import { comparePositive, votesIntoPercentage, castActorVotes, percentBetweenTwo } from './main'
+
+describe("percentBetweenTwo", () => {
+  it('should calc percentage of difference', () => {
+    expect(percentBetweenTwo(100,10)).toBe(10);
+    expect(percentBetweenTwo(20,10)).toBe(50);
+    expect(percentBetweenTwo(5,10)).toBe(50);
+  })
+  it('should return 0 when there is no difference', () => {
+    expect(percentBetweenTwo(100,100)).toBe(0);
+  })
+})
+
+describe("(castActorVotes) fix positive and negative", () => {
+  it('should transform string into int', () => {
+    const gominho = {
+      "__id": "7b1dd3f58be97715e9e06475bb58fce5",
+      "timestamp": 1408396481826,
+      "name": "Gominho",
+      "description": "Fofoqueiro de Plantão",
+      "picture": "http://im.r7.com/record/files/2C96/1618/3F6E/369D/013F/72F7/CF15/5F4B/Gominho1.jpg",
+      "positive": "23249923",
+      "negative": "39587707"
+    }
+    const gominhoFixed = castActorVotes(gominho)
+    expect(gominhoFixed.positive).toBe(23249923)
+    expect(gominhoFixed.negative).toBe(39587707)
+  });
+
+  it('should add 0 where does not exist', () => {
+    const missingNegative = {
+      "__id": "3404c4a70e7704009cd1915a349189f4",
+      "timestamp": 1408396555971,
+      "name": "Andressa Urach",
+      "description": "Personalidade da mídia",
+      "picture": "http://im.r7.com/record/files/2C96/1618/3F6E/369D/013F/72EF/C598/41DC/Andressa1.jpg",
+      "positive": null,
+      "age": 32
+    }
+    const fixed = castActorVotes(missingNegative)
+    expect(fixed.negative).toBeDefined()
+    expect(fixed.negative).toBe(0)
+  });
+
+  it('should be 0 when null', () => {
+    const nullPositive = {
+      "__id": "3404c4a70e7704009cd1915a349189f4",
+      "timestamp": 1408396555971,
+      "name": "Andressa Urach",
+      "description": "Personalidade da mídia",
+      "picture": "http://im.r7.com/record/files/2C96/1618/3F6E/369D/013F/72EF/C598/41DC/Andressa1.jpg",
+      "positive": null,
+      "age": 32
+    }
+    const fixed = castActorVotes(nullPositive)
+    expect(fixed.positive).toBe(0)
+  });
+})
+
+
+describe("votesIntoPercentage", () => {
+
+  it('should find diff between positive and negative attribute', () => {
+    const before = [{
+      positive: 10,
+      negative: 10
+    }]
+    const expected = [{
+      positive: 50,
+      negative: 50
+    }]
+    expect(votesIntoPercentage(before)).toEqual(expected)
+  })
+
+  it('should find exact diff', () => {
+    const before = [{
+      positive: 51638022,
+      negative: 18143089
+    }]
+    const expected = [{
+      positive: 73.99999979937265,
+      negative: 26.00000020062736
+    }]
+    expect(votesIntoPercentage(before)).toEqual(expected)
+  })
+});
+
+describe("comparePositive", () => {
+  it("should return 1 when positive is bigger", () => {
+    expect(comparePositive(2,1)).toBe(1)
+  })
+})
